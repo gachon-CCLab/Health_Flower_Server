ef7b596696679828bb7665f1a10c583238b38384
Semo <tpah20@gmail.com>
2022-06-06 00:11:04 +0900

    kubernetes에 적용하기 위해 app.py로 변경

--- app.py
+++ app.py
@@ -16,15 +16,19 @@ import wandb
 from datetime import datetime, timedelta 
 import os
 import boto3
+
+import requests, json
+
 from dotenv import load_dotenv
 load_dotenv(verbose=True)
 
  # 날짜를 폴더로 설정
-global today_str, yesterday_str
-today= datetime.today()
-today_str = today.strftime('%Y-%m-%d')
-yesterday= today-timedelta(days=1)
-yesterday_str = yesterday.strftime('%Y-%m-%d')
+# global today_str, yesterday_str
+# today= datetime.today()
+# today_str = today.strftime('%Y-%m-%d')
+# today_time = today.strftime('%Y-%m-%d %H-%M-%S')
+# yesterday= today-timedelta(days=1)
+# yesterday_str = yesterday.strftime('%Y-%m-%d')
 
 
 # FL 하이퍼파라미터 설정
@@ -44,13 +48,13 @@ def aws_session(region_name='ap-northeast-2'):
 # s3에 global model upload
 def upload_model_to_bucket(global_model):
     bucket_name = os.getenv('AWS_BUCKET_NAME')
-    global today_str
+    global today_str, latest_gl_model_v, next_gl_model
     
     session = aws_session()
     s3_resource = session.resource('s3')
     bucket = s3_resource.Bucket(bucket_name)
     bucket.upload_file(
-        Filename='/Users/yangsemo/VScode/Flower_Health/%s_model.h5'%today_str,
+        Filename='/Users/yangsemo/VScode/Flower_Health/model_V%s.h5'%latest_gl_model_v,
         Key=global_model,
     )
     
@@ -61,7 +65,7 @@ def upload_model_to_bucket(global_model):
 # s3에 저장되어 있는 latest global model download
 def model_download():
     bucket_name = os.getenv('AWS_BUCKET_NAME')
-    global today_str, yesterday_str
+    global latest_gl_model_v, next_gl_model
     
     session = aws_session()
     s3_resource = session.client('s3')
@@ -75,22 +79,25 @@ def model_download():
     for content in content_list:
         key = content['Key']
         file_list.append(key)
+    
+    model = s3_resource.download_file(bucket_name,'model_V%s.h5'%latest_gl_model_v, '../download_model/model_V%s.h5'%latest_gl_model_v)
 
-    if '%s_model.h5'%today_str in file_list:
-        model = s3_resource.download_file(bucket_name,'%s_model.h5'%today_str, '../download_model/%s_model.h5'%today_str)
-    else:
-        model = s3_resource.download_file(bucket_name,'%s_model.h5'%yesterday_str, '../download_model/%s_model.h5'%yesterday_str)
+
+    # if 'model_V%s.h5'%latest_gl_model_v in file_list:
+    #     model = s3_resource.download_file(bucket_name,'model_V%s.h5'%latest_gl_model_v, '../download_model/model_V%s.h5'%latest_gl_model_v)
+    # else:
+    #     model = s3_resource.download_file(bucket_name,'%s_model.h5'%yesterday_str, '../download_model/%s_model.h5'%yesterday_str)
+    #     # model = s3_resource.download_file(bucket_name,'2022-05-23_model.h5', '../download_model/2022-05-23_model.h5')
+
     
     return model
 
-def main() -> None:
-
-    global num_rounds
-    global today_str, yesterday_str
+def fl_server_start(model):
 
     # Load and compile model for
     # 1. server-side parameter initialization
     # 2. server-side parameter evaluation
+
     METRICS = [
         tf.keras.metrics.BinaryAccuracy(name='accuracy'),
         tf.keras.metrics.Precision(name='precision'),
@@ -98,19 +105,7 @@ def main() -> None:
         tf.keras.metrics.AUC(name='auc'),
         # tfa.metrics.F1Score(name='f1_score', num_classes=5),
         tf.keras.metrics.AUC(name='auprc', curve='PR'), # precision-recall curve
-    ]
-
-    # model = tf.keras.Sequential([
-    #     tf.keras.layers.Dense(
-    #         16, activation='relu',
-    #         input_shape=(6,)), # 학습 고려 컬럼 6개(bpm, spo2, rr, sbp, dbp,mbp)
-    #     tf.keras.layers.Dropout(0.5),
-    #     tf.keras.layers.Dense(5, activation='sigmoid'),
-    # ])
-    if os.path.isfile('../download_model/%s_model.h5'%today_str):
-        model = tf.keras.models.load_model('../download_model/%s_model.h5'%today_str)
-    else:
-        model = tf.keras.models.load_model('../download_model/%s_model.h5'%yesterday_str)
+        ]
 
     model.compile(
         optimizer=tf.keras.optimizers.Adam(learning_rate=1e-3),
@@ -134,7 +129,47 @@ def main() -> None:
     # Start Flower server for four rounds of federated learning
     fl.server.start_server("[::]:8080", config={"num_rounds": num_rounds}, strategy=strategy)
 
+def main() -> None:
 
+    global num_rounds, latest_gl_model_v
+    global today_str, yesterday_str
+
+    print('')
+    print('latest_gl_model_v', latest_gl_model_v)
+    print('')
+
+    
+    if os.path.isfile('../download_model/model_V%s.h5'%latest_gl_model_v):
+        print('load model')
+        model = tf.keras.models.load_model('../download_model/model_V%s.h5'%latest_gl_model_v)
+        fl_server_start(model)
+
+    else:
+        print('basic model making')
+        
+        METRICS = [
+            tf.keras.metrics.BinaryAccuracy(name='accuracy'),
+            tf.keras.metrics.Precision(name='precision'),
+            tf.keras.metrics.Recall(name='recall'),
+            tf.keras.metrics.AUC(name='auc'),
+            # tfa.metrics.F1Score(name='f1_score', num_classes=5),
+            tf.keras.metrics.AUC(name='auprc', curve='PR'), # precision-recall curve
+        ]
+
+        model = tf.keras.Sequential([
+            tf.keras.layers.Dense(
+                16, activation='relu',
+                input_shape=(6,)),
+                tf.keras.layers.Dropout(0.5),
+            tf.keras.layers.Dense(5, activation='sigmoid'),
+        ])
+        model.compile(
+        optimizer=tf.keras.optimizers.Adam(learning_rate=1e-3),
+        loss=tf.keras.losses.BinaryCrossentropy(),
+        metrics=METRICS)
+
+        fl_server_start(model)
+        
 
 def get_eval_fn(model):
     """Return an evaluation function for server-side evaluation."""
@@ -157,13 +192,10 @@ def get_eval_fn(model):
         loss, accuracy, precision, recall, auc, auprc = model.evaluate(x_val, y_val)
         # loss, accuracy, precision, recall, f1_score, auc, auprc = model.evaluate(x_val, y_val)
 
-        global today_str
+        global next_gl_model, res
 
         # model save
-        model.save("../%s_model.h5" % today_str)
-
-        # s3 버킷에 global model upload
-        upload_model_to_bucket("%s_model.h5" % today_str)
+        model.save("../model_V%s.h5"%next_gl_model)
 
         # wandb에 log upload
         wandb.log({'loss':loss,"accuracy": accuracy, "precision": precision, "recall": recall, "auc": auc})
@@ -216,20 +248,56 @@ def evaluate_config(rnd: int):
 
 if __name__ == "__main__":
 
+    
+    today= datetime.today()
+    today_time = today.strftime('%Y-%m-%d %H-%M-%S')
+
+    # server_status 주소
+    inform_SE: str = 'http://0.0.0.0:8000/FLSe/'
+
+    # server_status 확인 => 전 global model 버전
+    server_res = requests.get(inform_SE + 'info')
+    latest_gl_model_v = int(server_res.json()['Server_Status']['GL_Model_V'])
+    
+    # 다음 global model 버전
+    next_gl_model = latest_gl_model_v + 1
+
+    inform_Payload = {
+            # 형식
+            'S3_bucket': 'fl-flower-model', # 버킷명
+            'S3_key': 'model_V%s.h5'%latest_gl_model_v,  # 모델 가중치 파일 이름
+            'play_datetime': today_time, # server 수행 시간
+            'FLSeReady': True, # server 준비 상태 on
+            'Model_V' : latest_gl_model_v # GL 모델 버전
+        }
+
+    # server_status => FL server ready
+    requests.put(inform_SE+'FLSeUpdate', data=json.dumps(inform_Payload))
+    
     # wandb login and init
     wandb.login(key=os.getenv('WB_KEY'))
     # wandb.init(entity='ccl-fl', project='health_flower', name='health_acc_loss v2')
-    wandb.init(entity='ccl-fl', project='server_flower', name= 'server_flower v7', dir='/Users/yangsemo/VScode/Flower_Health/wandb_server',  \
-        config={"num_rounds": num_rounds,"local_epochs": local_epochs, "batch_size": batch_size,"val_steps": val_steps})
+    wandb.init(entity='ccl-fl', project='server_flower', name= 'server_V%s'%next_gl_model, dir='/Users/yangsemo/VScode/Flower_Health/wandb_server',  \
+        config={"num_rounds": num_rounds,"local_epochs": local_epochs, "batch_size": batch_size,"val_steps": val_steps, "today_datetime": today_time,
+        "Model_V": next_gl_model})
+
     
     # s3에서 latest global model 가져오기
-    model_download()
+    # model_download()
     
     try:
         # Flower Server 실행
         main()
 
-         # wandb 종료
-        wandb.finish()
+        # server_status에 model 버전 수정 update request
+        res = requests.put(inform_SE + 'FLRoundFin', params={'FLSeReady': 'false'})
+
+        # s3 버킷에 global model upload
+        upload_model_to_bucket("model_V%s.h5" %next_gl_model)
+
+         
     finally:
         print('server close')
+
+        # wandb 종료
+        wandb.finish()
