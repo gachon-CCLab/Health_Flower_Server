6a4b3587d3cd1a04819e5d3a97f8808527c91d36
Semo Yang <57698795+YangSemo@users.noreply.github.com>
2022-06-20 17:01:25 +0900

    1

--- app.py
+++ app.py
@@ -62,7 +62,7 @@ def upload_model_to_bucket(global_model):
 # s3에 저장되어 있는 latest global model download
 def model_download():
     bucket_name = os.environ.get('BUCKET_NAME')
-    print(bucket_name: ', bucket_name)
+    print('bucket_name: ', bucket_name)
     global latest_gl_model_v, next_gl_model
     
     session = aws_session()
